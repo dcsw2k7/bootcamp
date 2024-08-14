a4951add39efacf8bcaae4c1409795113d4ce33b
dicky chan <dcsw2k7@gmail.com>
2024-08-14 17:39:02 +0800

    update

---
+++ TEST-Quiz/src/Q1/Quiz1Q2.java
@@ -0,0 +1,10 @@
+package Q1;
+
+public class Quiz1Q2 {
+  public static void main(String[] args) {
+    byte hoursOfDay = 24;
+    byte DaysOfWeek = 7;
+    byte hoursOfWeek = (byte) (hoursOfDay * DaysOfWeek);
+    System.out.println(hoursOfWeek);
+  }
+}
--- TEST-dc/src/C_byte.java
+++ TEST-dc/src/C_byte.java
@@ -2,8 +2,11 @@ public class C_byte {
   public static void main(String[] args) {
     byte i = 127;
     System.out.println(i);
-    byte i2 = i + 1;
+    byte i2 = -128;
     System.out.println(i2);
+    byte i3 = -127 + 1;
+    //System.out.println(i3);
+
 
   }
 }
